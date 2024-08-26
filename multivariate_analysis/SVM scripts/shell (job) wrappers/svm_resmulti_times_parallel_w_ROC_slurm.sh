b8875660c1896083c82d363d8685c9ee657d7e50
ashleychari <ashleychari1234@gmail.com>
2024-08-26 13:37:24 -0400

    slurm array job script

---
+++ multivariate_analysis/SVM scripts/shell (job) wrappers/svm_resmulti_times_parallel_w_ROC_slurm.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#SBATCH --mem=50G
+#SBATCH --array=1-100
+#SBATCH --time=1-00:00:00
+
+python3 /cbica/projects/ash_pfn_sex_diff_abcd/dropbox/run_2fold_svm_resmulti_times_parallel_w_ROC.py -5 10 $SLURM_ARRAY_TASK_ID
+
+
