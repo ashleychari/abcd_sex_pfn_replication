d6616ba1ab7c5c37aebe0368c926715b30f67cfd
ashleychari <ashleychari1234@gmail.com>
2024-09-22 17:37:21 -0400

    slurm haufe transformation script

---
+++ multivariate_analysis/haufe transformation scripts/haufe_transform_weights_slurm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#SBATCH --mem=500G
+#SBATCH --time=12:00:00
+
+python3 /Users/ashfrana/Desktop/code/abcd_sex_pfn_replication/multivariate_analysis/haufe transformation scripts/haufe_transform_weights.py
