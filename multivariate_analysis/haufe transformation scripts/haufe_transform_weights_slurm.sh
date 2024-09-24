92f1b536964abb8925255471c030a4c83c9875c0
ashleychari <ashleychari1234@gmail.com>
2024-09-24 14:55:57 -0400

    change to correct path

--- multivariate_analysis/haufe transformation scripts/haufe_transform_weights_slurm.sh
+++ multivariate_analysis/haufe transformation scripts/haufe_transform_weights_slurm.sh
@@ -2,4 +2,4 @@
 #SBATCH --mem=500G
 #SBATCH --time=12:00:00
 
-python3 /Users/ashfrana/Desktop/code/abcd_sex_pfn_replication/multivariate_analysis/haufe transformation scripts/haufe_transform_weights.py
+python3 /cbica/projects/ash_pfn_sex_diff_abcd/dropbox/haufe_transform_weights.py
